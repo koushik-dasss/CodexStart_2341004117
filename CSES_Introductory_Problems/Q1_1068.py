6ed1db1cefb6e74c1bc594276e70c473bea37130
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-10-15 14:05:01 +0530

    Comment and code updated

--- CSES_Introductory_Problems/Q1_1068.py
+++ CSES_Introductory_Problems/Q1_1068.py
@@ -1,6 +1,23 @@
 ''' Name :- Koushik Das
     Regd. No.:- 2341004117
     Qs. Link:-  https://cses.fi/problemset/task/1068 '''
+
+''' Consider an algorithm that takes as input a positive integer 
+
+n. If 
+
+n is even, the algorithm divides it by two, and if 
+
+n is odd, the algorithm multiplies it by three and adds one. The algorithm repeats this, until 
+
+n is one. For example, the sequence for 
+n=3 is as follows:
+3→10→5→16→8→4→2→1
+
+Your task is to simulate the execution of the algorithm for a given value of 
+n.
+'''
+
 # Taking input (1<=n<=10^6)
 num = int(eval(input("Enter an integer: ")))
 false = 0
