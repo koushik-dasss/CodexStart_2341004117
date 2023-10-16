19a4913eb466a6c92a310603967f5c96e68bbcf1
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-10-16 23:10:58 +0530

    Added code

--- CSES_Introductory_Problems/Q2_1083.py
+++ CSES_Introductory_Problems/Q2_1083.py
@@ -1,13 +1,17 @@
 n = int(input())
+given_numbers =[]
 ctr =1
-given_numbers = list(map(int, input().split()))
-    
-new_sum = sum(given_numbers)
+while ctr<n:
+    number = int(input())
+    given_numbers.append(number)
+    ctr+=1
 
+new_sum =0
+for i in given_numbers:
+    new_sum+=i
 total_sum = 0
 for i in range(1,n+1):
-    total_sum+=1
-
+    total_sum+=i
 missing_number = total_sum - new_sum
-print(missing_number)
 
+print(missing_number)
