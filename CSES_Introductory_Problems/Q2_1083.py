a0fa6a9c374cda553f234de142a6d5b590fb3cb2
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-10-16 18:57:27 +0530

    Comments deleted , to be updated

--- CSES_Introductory_Problems/Q2_1083.py
+++ CSES_Introductory_Problems/Q2_1083.py
@@ -1,4 +1,4 @@
-# Input
+
 n = int(input())
 given_numbers =[]
 ctr =1
@@ -10,16 +10,15 @@ for i in given_numbers:
     sum+=i
 
 
-# Calculate the expected sum using arithmetic progression formula
+
 expected_sum = n * (n + 1) // 2
 
-# Initialize the sum of given numbers
-
-# Loop through the given numbers and add them to the sum
 
 
-# Find the missing number
+
+
+
+
 missing_number = expected_sum - sum
 
-# Output
 print(missing_number)
