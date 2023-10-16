2be6417f34f071d2136d3d2fdaec9d8beb3271a8
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-10-16 19:47:24 +0530

    Added again

--- CSES_Introductory_Problems/Q2_1083.py
+++ CSES_Introductory_Problems/Q2_1083.py
@@ -1,24 +1,15 @@
-
 n = int(input())
 given_numbers =[]
 ctr =1
 while ctr<n:
     number = int(input())
-    given_numbers.append()
+    given_numbers.append(number)
+    ctr+=1
+    
 sum =0
 for i in given_numbers:
     sum+=i
 
-
-
 expected_sum = n * (n + 1) // 2
-
-
-
-
-
-
-
 missing_number = expected_sum - sum
-
 print(missing_number)
