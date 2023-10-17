aadf01b8e78d16f496ac2dc6de7102578deb14b2
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-10-17 23:15:10 +0530

    Done

--- CSES_Introductory_Problems/Q2_1083.py
+++ CSES_Introductory_Problems/Q2_1083.py
@@ -1,21 +1,11 @@
 ''' Name :- Koushik Das
     Regd. No.:- 2341004117
     Qs. Link:-  https://cses.fi/problemset/task/1068 '''
-
-
-
 ''' You are given all numbers between  1,2,…n
  except one. Your task is to find the missing number.
-
 input = n 
         (n-1) numbers in one line
 output = missing number '''         
-
-
-
-
-
-
 # Input n (2≤n≤2*10^5))
 n = int(input())
 list_numbers=[]  # Empty list initialisation 
@@ -27,10 +17,8 @@ while ctr<n:
     ctr+=1
 for i in list_numbers:
     new_sum+=i  # sum of (n-1) numbers
-
 total_sum = 0
 for i in range(1,n+1):
     total_sum+=i # sum of n numbers
 missing_number = total_sum - new_sum   # output
-
 print(missing_number)
--- CSES_Introductory_Problems/Q3_1069.py
+++ CSES_Introductory_Problems/Q3_1069.py
@@ -3,21 +3,12 @@
     Qs. Link:-  https://cses.fi/problemset/task/1069 '''
 # Input of DNA Sequence
 DNA_Sequence = input()  
-
-
-
-
 r = 1 # Initialisation of varibale for normal repetition 
 max_r = 1 # Initialisation of varibale for highest repetition 
-
-
 for i in range(1, len(DNA_Sequence)): # iterating each character of the DNA Sequence 
     if DNA_Sequence[i] == DNA_Sequence[i - 1]:  # Character equal or not 
         r += 1
     else:
         max_r = max(max_r,r) 
         r=1
-        
-
-
 print(max(max_r, r)) # Output ( Longest repitition in the sequence )
