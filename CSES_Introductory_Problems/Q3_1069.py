c7ae914fca71bbb7262083f0573bb3c4d4f4f67f
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-10-17 22:45:21 +0530

    Comment added + code updated for Q_3 1069.py

--- CSES_Introductory_Problems/Q3_1069.py
+++ CSES_Introductory_Problems/Q3_1069.py
@@ -1,22 +1,23 @@
 ''' Name :- Koushik Das
     Regd. No.:- 2341004117
     Qs. Link:-  https://cses.fi/problemset/task/1069 '''
-
-DNA_Sequence = input()
-
-
+# Input of DNA Sequence
+DNA_Sequence = input()  
 
 
-r = 1
-max_r = 1
 
-for i in range(1, len(DNA_Sequence)):
-    if DNA_Sequence[i] == DNA_Sequence[i - 1]:
+
+r = 1 # Initialisation of varibale for normal repetition 
+max_r = 1 # Initialisation of varibale for highest repetition 
+
+
+for i in range(1, len(DNA_Sequence)): # iterating each character of the DNA Sequence 
+    if DNA_Sequence[i] == DNA_Sequence[i - 1]:  # Character equal or not 
         r += 1
     else:
-        max_r = max(max_r,r)
+        max_r = max(max_r,r) 
         r=1
         
 
 
-print(max(max_r, r))
+print(max(max_r, r)) # Output ( Longest repitition in the sequence )
