b9a019248205f79a4b68e2443dd9f095e17952ff
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-10-17 18:03:28 +0530

    Comment added + I can't solve this question in any other way like using different methods to get the (n-1) numbers in one line , so I am submitting this code. I don't want to cpy from anywhere because I have this much of honest knowledge only . Out of 14 test cases on CSES website , I am getting my logic passed in 2 test cases and other one are having runtime error as I can't take input of (n-1) integers in one line

--- CSES_Introductory_Problems/Q2_1083.py
+++ CSES_Introductory_Problems/Q2_1083.py
@@ -1,17 +1,36 @@
+''' Name :- Koushik Das
+    Regd. No.:- 2341004117
+    Qs. Link:-  https://cses.fi/problemset/task/1068 '''
+
+
+
+''' You are given all numbers between  1,2,…n
+ except one. Your task is to find the missing number.
+
+input = n 
+        (n-1) numbers in one line
+output = missing number '''         
+
+
+
+
+
+
+# Input n (2≤n≤2*10^5))
 n = int(input())
-list_numbers=[]
+list_numbers=[]  # Empty list initialisation 
 new_sum =0
 ctr =1
 while ctr<n:
-    number = int(input())
-    list_numbers.append(number)
+    number = int(input())   
+    list_numbers.append(number)   # (n-1) numbers appended in the list (including n and other numbers between n)
     ctr+=1
 for i in list_numbers:
-    new_sum+=i
+    new_sum+=i  # sum of (n-1) numbers
 
 total_sum = 0
 for i in range(1,n+1):
-    total_sum+=i
-missing_number = total_sum - new_sum
+    total_sum+=i # sum of n numbers
+missing_number = total_sum - new_sum   # output
 
 print(missing_number)
